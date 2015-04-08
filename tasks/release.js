bf1e5e1f84ebd7b238e8f79e3c39039a56257edb
José Cieni <jose.cieni@movile.com>
2015-04-08 17:21:35 -0300

    Removed yargs dependency

--- tasks/release.js
+++ tasks/release.js
@@ -1,78 +1,52 @@
-module.exports = function (gulp) {
-    var paths, versioning;
+var fs = require('fs');
+var prompt = require('gulp-prompt');
+var git = require('gulp-git');
+var bump = require('gulp-bump');
+var filter = require('gulp-filter');
+var tag_version = require('gulp-tag-version');
+var _ = require('lodash');
 
-    var fs = require('fs');
-    var prompt = require('gulp-prompt');
-    var git = require('gulp-git');
-    var bump = require('gulp-bump');
-    var filter = require('gulp-filter');
-    var tag_version = require('gulp-tag-version');
-    var argv = require('yargs').argv;
+module.exports = function (options, gulp) {
 
-    var versioningFiles = function () {
-        if (argv.bower) {
-            return ['./bower.json']
-        }
-        if (argv.npm || argv.node) {
-            return ['./package.json']
-        }
-        if (fs.existsSync('package.json') && fs.existsSync('bower.json')) {
-            return ['./package.json', './bower.json']
-        } else {
-            return ['./package.json']
-        }
-    };
-
-    var bumpPreference = fs.existsSync('package.json') ? 'package.json' : 'bower.json';
-    var vFiles = versioningFiles();
-
-    paths = {
-        versionsToBump: vFiles,
-        version: bumpPreference,
-        dest: '.'
-    };
+    options = _.defaults({
+        filesToBump: [ './package.json', './bower.json' ],
+        referenceFile: './package.json',
+        destination: './',
+        version: null,
+        releaseType: 'patch'
+    }, options);
 
     gulp.task('tag', ['bump', 'commit'], function () {
-        return gulp.src(paths.versionsToBump)
-            .pipe(filter(paths.version))
+        return gulp.src(options.filesToBump)
+            .pipe(filter(options.referenceFile))
             .pipe(tag_version())
             .pipe(git.push('origin', 'master', {
                 args: '--tags'
             }));
     });
 
-    gulp.task('add', function() {
-        return gulp.src(paths.versionsToBump)
+    gulp.task('add', function () {
+        return gulp.src(options.filesToBump)
             .pipe(git.add());
     });
 
-    gulp.task('commit', ['add'], function() {
-        return gulp.src(paths.version)
+    gulp.task('commit', ['add'], function () {
+        return gulp.src(options.referenceFile)
             .pipe(prompt.prompt({
                 type: 'input',
                 name: 'commit',
                 message: 'enter a commit msg, eg initial commit'
-            }, function(res) {
-                return gulp.src('.')
+            }, function (res) {
+                return gulp.src(options.destination)
                     .pipe(git.commit(res.commit));
             }));
     });
 
-    versioning = function() {
-        if (argv.minor || argv.feature) {
-            return 'minor';
-        }
-        if (argv.major) {
-            return 'major';
-        }
-        return 'patch';
-    };
-
-    gulp.task('bump', function() {
-        return gulp.src(paths.versionsToBump)
+    gulp.task('bump', function () {
+        return gulp.src(options.filesToBump)
             .pipe(bump({
-                type: versioning()
+                type: options.releaseType
             }))
-            .pipe(gulp.dest(paths.dest));
+            .pipe(gulp.dest(options.destination));
     });
 };
